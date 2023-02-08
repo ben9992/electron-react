9a27fe967f7d093a2bbbd4f29e3d0a2eeeb62fc4
Ben Mishali <you@example.com>
2023-02-08 19:15:54 +0200

    added build scripts

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
+const path = require("path");
+
 const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
 	// Create the browser window.
@@ -10,11 +13,17 @@ function createWindow() {
 		},
 	});
 
-	//load the index.html from a url
-	win.loadURL("http://localhost:3000");
-
+	// and load the index.html of the app.
+	// win.loadFile("index.html");
+	win.loadURL(
+		isDev
+			? "http://localhost:3000"
+			: `file://${path.join(__dirname, "../build/index.html")}`
+	);
 	// Open the DevTools.
-	win.webContents.openDevTools();
+	if (isDev) {
+		win.webContents.openDevTools({ mode: "detach" });
+	}
 }
 
 // This method will be called when Electron has finished
@@ -23,7 +32,7 @@ function createWindow() {
 app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
+// for applications and their menu bars to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") {
@@ -32,13 +41,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-
 	if (BrowserWindow.getAllWindows().length === 0) {
 		createWindow();
 	}
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
